f26ac72d01befd48148f5ded5416464057e08bfe
Gabriel Lucas <gabrielglpsi@gmail.com>
2023-11-30 18:06:33 -0300

    Projeto pronto para apresentar

--- utils/data.ts
+++
@@ -1,6 +0,0 @@
-export const navigationLinks = [
-    { label: 'Home', path: '/' },
-    { label: 'Veículos', path: '/veiculos' },
-    { label: 'Rastrear', path: '/rastrear' },
-    { label: 'Configurações', path: '/configuracoes' },
-];
